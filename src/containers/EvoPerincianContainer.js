55ec5ede5ae14f55f09e0e4677ed67f556583085
Rizqi Pratama <rev4rmy@gmail.com>
2021-05-17 17:07:29 +0700

    Menambahkan Firebase dan fix fungsi didmount

--- src/containers/EvoPerincianContainer.js
+++ src/containers/EvoPerincianContainer.js
@@ -5,57 +5,35 @@ import Row from "reactstrap/lib/Row";
 import EvoPerincianComponent from "../components/TableComponents/EvoPerincianComponent";
 import JumbotronComponent from "../components/JumbotronComponent";
 import EvoPerincianCloudComponent from "../components/TableComponents/EvoPerincianCloudComponent";
-import { getLTcardEvoPerincian, getTcardEvoPerincian } from "../actions/tcardAction";
-import firebase from '../firebase';
+import {
+  getLTcardEvoPerincian,
+  getTcardEvoPerincian,
+} from "../actions/tcardAction";
+import firebase from "../firebase";
 
 class EvoPerincianContainer extends Component {
-
   componentDidMount() {
-
-
-    // setInterval(() => {
-    //   this.props.dispatch(getLTcardEvoPerincian());
-    // }, 3000);
-    // 
-    // this.props.dispatch(getTcardEvoPerincian());
-    // setInterval(() => {
-    //   
-    // }, 3000);
-    firebase.database().ref('/evoperincian')
-      .on('value', snapshot => {
+    firebase
+      .database()
+      .ref("/evoperincian")
+      .on("value", (snapshot) => {
         const data = snapshot.val();
-        console.log(data)
+        console.log(data);
         this.props.dispatch(getTcardEvoPerincian());
         this.props.dispatch(getLTcardEvoPerincian());
-      });  
-
-
+      });
   }
 
-  // componentDidUpdate(){
-  //   this.props.dispatch(getLTcardEvoPerincian());
-  //   this.props.dispatch(getTcardEvoPerincian());
-  // }
-
-
-
-
-  // componentDidUpdate(){
-  //   this.props.dispatch(getTcardEvoPerincian());
-  //   this.props.dispatch(getLTcardEvoPerincian());
-  // }
-
   render() {
-  
     return (
       <div>
-        <JumbotronComponent/>
-        <Row >
-          <Col md={6}>    
-              <EvoPerincianComponent/>
+        <JumbotronComponent />
+        <Row>
+          <Col md={6}>
+            <EvoPerincianComponent />
           </Col>
           <Col md={6}>
-              <EvoPerincianCloudComponent/>
+            <EvoPerincianCloudComponent />
           </Col>
         </Row>
       </div>
